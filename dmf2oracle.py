46eaf8abf1dfa134b78be450fd475f6e89ce547d
jamespmanning <james.patrick.manning@gmail.com>
2025-02-18 18:15:37 -0500

    Add files via upload

--- dmf2oracle.py
+++ dmf2oracle.py
@@ -1,41 +1,63 @@
 # routine to process dmf shipwreck temps
+# since the data comes in different formats every year, the code needs to deal with various input options
+# sometimes DMF sends us the data with multiple columns and sometimes it comes in multiple sheets
+# Most recently, in 2025, it is the later with each site is stored in a separate sheet
 import numpy as np
 from datetime import datetime,timedelta
 from pandas import read_csv,DataFrame,read_excel
-sites=['Cleveland Ledge','Buzzards Bay Barge','Rocky Point','Manomet Boulders','Wreck of the Mars','Martins Ledge','Sippiwisset Rock','Scortons Ledge','Cuttyhunk']
-#BUZ	BUZ	    CCB	    CCB	    CCB	    CCB	    BOS	         BUZ	    BOS	     CCB	      BUZ
-#CLEV   TOWR  	RCKY	MNMT	ENDC	MARS	RMNC/Martins	 Sipp	Sculpin	 Scortons CUTTY
-# 7     6       5       4       3       2       1            10     8         9       11
-#directory='/net/data5/jmanning/newt/mass_dmf/2015/'
+yr=2025 # year of processing
+#sites=['Cleveland Ledge','Buzzards Bay Barge','Rocky Point','Manomet Boulders','Wreck of the Mars','Martins Ledge','Sippiwisset Rock','Scortons Ledge','Cuttyhunk']
+
+#BUZ	    BUZ	        CCB	 CCB	    CCB	    CCB	    BOS	         BUZ	    BOS	     CCB	      BUZ
+#CLEV   TOWR/Barge  	RCKY	 MNMT	ENDC 	MARS 	RMNC/Martins	 Sipp	Sculpin	 Scortons CUTTY
+# 7     6           5    4      3        2       1            10     8         9       11
+#directory='/net/data5/jmanning/newt/mass_dmf/2015/' #originally stored data here
 directory='' # puts input and output file in same directory as code
-wsite=[7,6,5,4,2,1,10,9,11]# site code associated with "sites" above (note: 3 ENDC in 13.5Fa & 8 Sculpin in 3Fa not here anymore)
+# site code associated with "sites" above (note: 3 ENDC in 13.5Fa & 8 Sculpin in 3Fa not here anymore)
 #depths=[4.2,13.7,6.4,9.3,18.5,14.5,3,2,999] # water depth in fathoms based on 2020 from Derek
-depths=[4.2,11.6,7.5,9.2,18.3,11.6,3.0,5.0,3.0] # water depth in fathoms based on 2023 data from Alex
-depth_i=depths # instrument depth
-inputfile='DMF Temp Update ERRDAP 2023_scorton_fixed.xlsx' # file sent by ALex B in jan 2023
+ # water depth in fathoms based on 2023 data from Alex
+
+if yr==2024:
+    inputfile='DMF Temp Update ERRDAP 2023_scorton_fixed.xlsx' # file sent by ALex B in jan 2023
+    colnames=['SITE','Date','Time','Latitude','Longitude','Depth','Sea_Water_Temperature']
+    sites=['Cleveland','Barge','Rocky Point','Manomet','Mars','Martins','Sippiwissett','Scortons','Cuttyhunk']
+    wsite=[7,6,5,4,2,1,10,9,11]
+    depths=[4.2,11.6,7.5,9.2,18.3,11.6,3.0,5.0,3.0] # depths from Alex
+elif yr==2025:
+    inputfile='2025NOAATempUpdateforJM_JD.xlsx' # file sent by Jake/Jacob Doherty
+    colnames=['SITE','Date','Hour','Latitude','Longitude','Depth','Sea_Water_Temperature']
+    sites=['Cleveland','Barge','Rocky Point','Manomet','Mars','Martins','Scortons','Sippiwissett','Cuttyhunk']
+    wsite=[7,6,5,4,2,1,9,10,11]# note 9 & 10 are switched relative to 2024 processing
+    depths=[4.2,11.6,7.5,9.2,18.3,11.6,5.0,3.0,3.0]
 def parse(datet1,datet2):
     ''' parses a few date columms that we have combined inside read_csv'''
-    #dt = datetime.strptime(datet1[0:8],'%Y-%m-%d')
+    dt = datetime.strptime(datet1[0:8],'%Y-%m-%d')
     print(datet1,datet2)
     dt=datet1
     #delta = timedelta(hours=int(datet2[20:22]))
     delta=datet2
     return dt + delta
+depth_i=depths # instrument depth
 #tso=read_csv(directory+'tempsall.csv',parse_dates={'datet':['DATE','HR']},index_col='datet',date_parser=parse,skiprows=0,header=1)
 #numsites=tso.shape[1]
 numsites=9
 #orig_name=tso.columns.values
-for k in [7]:#range(numsites): # if you want specific one like "ROCKY" specify "k in [2]:", for example
+for k in range(numsites): # if you want specific one like "ROCKY" specify "k in [2]:", for example
     print(sites[k])
     # create a dataframe for this site
     #tsdf=DataFrame(tso[orig_name[k]],index=tso.index)
-    #tsdf=read_excel(directory+inputfile,parse_dates={'datet':['Date','Time']},index_col='datet',date_parser=parse,converters={'Date':str,'Time':str},sheet_name=sites[k])
-    colnames=['SITE','Date','Time','Latitude','Longitude','Depth','Sea_Water_Temperature']# column as shown in the first tab
+    #tsdf=read_excel(directory+inputfile,parse_dates={'datet':['Date','Hour']},index_col='datet',date_parser=parse,converters={'Date':str,'Hour':str},sheet_name=sites[k])
+    #tsdf=read_excel(directory+inputfile,parse_dates={'datet':['Date','Hour']},index_col='datet',date_parser=parse,converters={'Date':str,'Hour':str},sheet_name=sites[k])
     tsdf=read_excel(directory+inputfile,sheet_name=sites[k],header=None,skiprows=1,names=colnames)
-    try:
-        tsdf['hour']=tsdf['Time'].dt.hour # most often the case where time is formatted as datetime
+    # noticed in Fen 2025 that this automatically interprets date and time as datetime objects
+    tsdf.dropna(inplace=True)
+    if yr==2025:
+        tsdf.rename(columns={'Hour':'Time'},inplace=True) # to be consistent with previous years
+    #try:
+    tsdf['hour']=tsdf['Time']#.dt.hour # most often the case where time is formatted as datetime
+    '''
     except:
-        # case where the "time" column is no all datetimes as in the case of Scorton Ledge (float) and Martins Ledge where I formatted to "time"           
+        # 2024 processing special case needed as in the case of Scorton Ledge (float) and Martins Ledge where I formatted to "time"           
         hr=[]
         for kk in range(len(tsdf)):
             if type(tsdf['Time'][kk])==np.float64: # Scorton Ledge
@@ -43,19 +65,33 @@ for k in [7]:#range(numsites): # if you want specific one like "ROCKY" specify "
             else: # Martins ledge case where it is formatted as "time" 
                 hr.append(tsdf['Time'][kk].hour)
         tsdf['hour']=hr    
+    '''
     datet=[]
-    for j in range(len(tsdf)): 
-        if (tsdf['Date'][j]>datetime(2019,9,29,0,0,0)) and (k==7): # special case for Scortons ledge
+    
+    for j in range(len(tsdf)): # here's where we combine date and hour to make a datetime
+        '''
+        if (tsdf['Date'][j]>datetime(2019,9,29,0,0,0)) and (k==7) and (yr!=2025): # special 2024 case for Scortons ledge
             datet.append(tsdf['Date'][j])
         else:
-            datet.append(tsdf['Date'][j]+timedelta(hours=int(tsdf['hour'].values[j])))
+            '''
+        try:
+            datet.append(tsdf['Date'][j]+timedelta(hours=tsdf['hour'][j].hour))
+        except:
+            datet.append(np.nan)# this was needed in the case of Cuttyhunk having missing data 
+            print(sites[k]+' has trouble at ',tsdf.Date[j-1])
+
     tsdf['datet']=datet
+    tsdf.dropna(inplace=True)
     tsdf=tsdf.set_index('datet')
     # at some point in the future we may want to call "getemolt_temp" to determine the last date of the oracle for this site and then delimit the dataframe for that period only
     formatted_date,temp=[],[]
     for i in range(len(tsdf)):
       formatted_date.append(tsdf.index[i].strftime('%d-%b-%Y:%H:%M')) #get date in the format wanted by our Oracle loading routine
-      temp.append(float(1.8*tsdf['Sea_Water_Temperature'].values[i]+32.)) # converts to degF
+      try:
+          temp.append(float(1.8*tsdf['Sea_Water_Temperature'].values[i]+32.)) # converts to degF
+      except:
+          print(sites[k]+' has trouble at ',tsdf.index[i].strftime('%d-%b-%Y:%H:%M'))
+          temp.append(np.nan)
     tsdf['fd']=formatted_date
     tsdf['yd']=tsdf.index.dayofyear+tsdf.index.hour/24.+tsdf.index.minute/60./24.-1.0 #creates a yrday0 field
     tsdf['temp']=temp
@@ -71,7 +107,7 @@ for k in [7]:#range(numsites): # if you want specific one like "ROCKY" specify "
     output_fmt=['site','sn','probe_setting','fd','yd','temp','salt','depth_i']
     tsdfp=tsdf.reindex(columns=output_fmt)
     if wsite[k]<10:
-       tsdfp.to_csv(directory+'mabihourly_pre_oracle_DMF'+str(wsite[k])+'_2023.csv',index=False,header=False,float_format='%10.4f')
+       tsdfp.to_csv('oracle_ready/mabihourly_pre_oracle_DMF'+str(wsite[k])+'_'+str(yr)+'.csv',index=False,header=False,float_format='%10.4f')
     else:
-       tsdfp.to_csv(directory+'mabihourly_pre_oracle_MA'+str(wsite[k])+'_2023.csv',index=False,header=False,float_format='%10.4f')
+       tsdfp.to_csv('oracle_ready/mabihourly_pre_oracle_MA'+str(wsite[k])+'_'+str(yr)+'.csv',index=False,header=False,float_format='%10.4f')
       
--- plt_dmf_ts.py
+++ plt_dmf_ts.py
@@ -4,9 +4,13 @@
 Created on Thu Nov 28 08:13:11 2024
 
 @author: JiM
-routine to plot eMOLT time series
+routine to plot MassDMF time series as extracted from either:
+    -MassGIS site download
+    -ready-for-oracle .dat files
+    -ERDDAP
+Jan 2025 added linear trend rate (taken from plt_emolt_annual.py)
 """
-site='Mars 110'
+
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -16,34 +20,70 @@ from conversions import fth2m
 
 #MASS DMF csv file
 df=pd.read_csv('MA_Ocean_Bottom_Temperature_Sensor_Data.csv',index_col='DATE')
+#df.dropna(inplace=True)
 df.index=pd.to_datetime(df.index,format='%m/%d/%Y %I:%M:%S %p')#-td(6*365/12)
 #df['DateT']=pd.to_datetime(df['DATE'],format='%m/%d/%Y %I:%M:%S %p')-td(6*365/12)# where we need to center the year
 #df=df.set_index('DateT')
 
-# make time series plot
-fig=plt.figure(figsize=(10,8))
-ax=fig.add_subplot(111)
-ax.plot(df[site],label='bi-hourly temperatures')
+sites=df.columns.values
+wsite=[7,6,5,4,3,2,1,10,8,9,11]#emolt codes for "sites" listed
 
-# make annual mean & plot
-#df['Year_mean'] = df.groupby(df.index.year)[site].transform('mean')# value for each day (ie step function)
-dfa=df[site].resample('YE-JUN').mean() # centered around June?
-ax.plot(dfa.index[2:-2],dfa[2:-2],linewidth=4,label='annual mean')# step function
-
-
-# add another axis for Fahrenheit & label
-ax.set_ylabel('degC',fontsize=18)
-ax4=ax.twinx()
-ax4.set_ylabel('degF',fontsize=18)
-mint=np.nanmin(df[site].values)
-maxt=np.nanmax(df[site].values)
-#ax4.set_ylim((mint-32)/1.8,(maxt-32)/1.8)
-ax4.set_ylim(mint*1.8+32,maxt*1.8+32)
-ax.set_ylim(mint,maxt)
-
-ax.legend()
-plt.title('example Mass DMF data from shipwreck '+site+' feet ('+'%0.1f' % fth2m(float(site[-3:])/6)+' meters)')
-plt.savefig(site)
+for k in range(1,len(sites)):
+#for k in [7]:    
+    # make time series plot
+    print(sites[k])
+    fig=plt.figure(figsize=(10,8))
+    ax=fig.add_subplot(111)
+    ax.plot(df[sites[k]],label='MASS_GIS-served daily averages')
+    
+    if (wsite[k-1]!=3) and (wsite[k-1]!=8):
+        # add oracle-ready files
+        if wsite[k-1]<10:
+            dfo=pd.read_csv('oracle_ready/mabihourly_pre_oracle_DMF'+str(wsite[k-1])+'_2025.csv',header=None,index_col=3)
+        else:
+            dfo=pd.read_csv('oracle_ready/mabihourly_pre_oracle_MA'+str(wsite[k-1])+'_2025.csv',header=None,index_col=3)
+        dfo.index=pd.to_datetime(dfo.index,format='%d-%b-%Y:%H:%M')
+        dfo['degc']=(dfo[5]-32)/1.8
+        ax.plot(dfo['degc'],label='raw_pre_oracle bi-hourly')
+    
+    # make annual mean & plot with trend
+    #df['Year_mean'] = df.groupby(df.index.year)[site].transform('mean')# value for each day (ie step function)
+    dfa=df[sites[k]].resample('YE-JUN').agg(['mean', 'count'])
+    # determine invalid months
+    invalid = dfa['count'] <= 0.9*365 # only accepting years with at least 90% coverage
+    dfa = dfa['mean']
+    dfa[invalid] = np.nan
+    dfa.dropna(inplace=True)
+    #dfa=dfa[3:-2]
+    ax.plot(dfa.index,dfa,linewidth=4,color='r',label='annual mean')# step function
+    t = np.arange(0, len(dfa))
+    z = np.polyfit(t, dfa.values.flatten(), 1)
+    p = np.poly1d(z)
+    fitpts=p(t)
+    y=[fitpts[0],fitpts[-1]]
+    ax.plot([dfa.index[0],dfa.index[-1]],y,'--',color='r',linewidth=6,label='warming of '+chr(0x2191)+'%0.2f' % z[0]+' degC/year')
+    
+    
+    # add another axis for Fahrenheit & label
+    ax.set_ylabel('degC',fontsize=18)
+    ax4=ax.twinx()
+    ax4.set_ylabel('degF',fontsize=18)
+    mint=np.nanmin(df[sites[k]].values)
+    maxt=np.nanmax(df[sites[k]].values)
+    #ax4.set_ylim((mint-32)/1.8,(maxt-32)/1.8)
+    ax4.set_ylim(mint*1.8+32,maxt*1.8+32)
+    ax.set_ylim(mint,maxt)
+    
+    ax.legend()
+    try:
+        plt.title('Mass DMF data from shipwreck '+sites[k]+' feet ('+'%0.1f' % fth2m(float(sites[k][-3:])/6)+' meters)')
+    except:
+        plt.title('Mass DMF data from shipwreck '+sites[k])
+    if wsite[k-1]<10:
+        plt.savefig('plots_gis_site_data/DMF'+str(wsite[k-1])+'_jt.png')
+    else:
+        plt.savefig('plots_gis_site_data/MA'+str(wsite[k-1])+'_jt.png')
+    
 
 
 
